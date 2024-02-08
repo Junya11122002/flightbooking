9828bc33d5062b0b75f479340c715570a1ccfcef
Junya Ogawa <meresukaikai123@gmail.com>
2024-02-07 20:58:43 -0600

    testing

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        // this is some random new changes
+    
         System.out.println("Hello, World!");
     }
 }
